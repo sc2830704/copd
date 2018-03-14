e2584b7ce137de3f4204b528eaea1181ccef07d3
sc2830704 <sc2830704@gmail.com>
2018-03-14 17:34:09 +0800

    remove redutant

--- COPDHealthCare/app/src/main/java/com/example/mitlab_raymond/copdhealthcare/Callback/SshCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.example.mitlab_raymond.copdhealthcare.Callback;
-
-/**
- * Created by mitlab_raymond on 2018/1/31.
- */
-
-public interface SshCallBack {
-    void processFinish(String response, String command);
-}
